a52b319215af42f3abc9f4ea843435301af6d621
Durgesh Kumar Tiwari <durgesh@substringtechnologies.com>
2025-10-05 17:41:34 +0530

    advance RAG, ETL pipeline and tool calling

---
+++ tool-calling-example/src/test/java/com/lcwd/tool/calling/ToolCallingExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lcwd.tool.calling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToolCallingExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
